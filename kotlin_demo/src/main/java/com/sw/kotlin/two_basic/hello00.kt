ed408082f0ddc8b8644b21ec533f34e66fc3827a
shiwang <shiwang@rong360.com>
2017-11-01 16:19:07 +0800

    update kotlin

--- kotlin_demo/src/main/java/com/sw/kotlin/two_basic/hello00.kt
+++ kotlin_demo/src/main/java/com/sw/kotlin/two_basic/hello00.kt
@@ -1,7 +1,9 @@
 package com.sw.kotlin.two_basic
 
+import java.io.BufferedReader
+
 /**
- * if是表达式
+ * 方法 if是表达式
  */
 fun max(a: Int, b: Int): Int {
     return if (a > b) a else b
@@ -14,6 +16,9 @@ fun max2(a: Int, b: Int): Int = if (a > b) a else b
 fun max3(a: Int, b: Int) = if (a > b) a else b
 
 
+/*
+* 变量
+* */
 val a1: String = "hello"
 
 val a2 = "hello"
@@ -23,6 +28,9 @@ val a3 = 100
 var a4: String = "hello"
 
 
+/*
+* 字符串模版
+* */
 fun aa(args: Array<String>) {
     val a = "kotlin"
     println("hello $a")
@@ -30,6 +38,9 @@ fun aa(args: Array<String>) {
 }
 
 
+/*
+* 类
+* */
 class Person(val name: String, var isMarried: Boolean)
 
 
@@ -53,6 +64,9 @@ class Rectangle2(val width: Int, val height: Int) {
 }
 
 
+/*
+* 枚举
+* */
 enum class Color {
     RED, ORANGE, YELLOW
 }
@@ -65,6 +79,9 @@ enum class Color2(val r: Int, val g: Int, val b: Int) {
 }
 
 
+/*
+* when  if
+* */
 fun test02(color: Color) =
         when (color) {
             Color.RED -> "red color"
@@ -94,8 +111,11 @@ fun test05(): String {
     }
 }
 
-
+/*
+* 类型智能转换 as is
+* */
 interface Expr
+
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
@@ -113,3 +133,104 @@ fun eval(e: Expr): Int {
 fun test06() {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
+
+
+fun eval2(e: Expr): Int =
+        when (e) {
+            is Num -> e.value
+            is Sum -> eval2(e.left) + eval2(e.right)
+            else -> throw IllegalArgumentException("error expr")
+        }
+
+
+fun eval3(e: Expr): Int =
+        when (e) {
+            is Num -> {
+                println("hello when")
+                e.value
+            }
+            is Sum -> eval3(e.left) + eval3(e.right)
+            else -> throw IllegalArgumentException("error expr")
+        }
+
+
+/*
+* while for
+* */
+fun testWhile() {
+    var i = 0
+    while (i < 10) {
+        println(i++)
+    }
+
+    do {
+        println(i++)
+    } while (i < 15)
+}
+
+fun testFor() {
+    for (i in 1..10) {
+        print(i)
+    }
+
+    for (i in 10 downTo 1 step 2) {
+        print(i)
+    }
+
+
+    val map = HashMap<String, String>()
+    map.put("aa", "11")
+    map.put("bb", "22")
+    map.put("cc", "33")
+
+    for ((k, v) in map) {
+        print("key:$k  value:$v")
+    }
+
+
+    val list = arrayListOf("10", "22", "cc")
+    for ((index, v) in list.withIndex()) {
+        print("key:$index  value:$v")
+    }
+
+}
+
+
+/*
+* 异常
+* */
+fun testExc(i: Int) {
+    if (i !in 1..100) {
+        throw IllegalArgumentException("error input i")
+    }
+}
+
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
+
+fun readNumber2(reader: BufferedReader) {
+    val num = try {
+        val line = reader.readLine()
+        Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        null
+    }
+    println(num)
+}
+
+
+
+
+
+
+
+
