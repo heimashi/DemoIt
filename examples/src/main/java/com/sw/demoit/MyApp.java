6ad341f702de355d52de2038a417a4250ae80fa3
shiwang <shiwang@rong360.com>
2017-11-27 14:04:07 +0800

    update ui

--- examples/src/main/java/com/sw/demoit/MyApp.java
+++ examples/src/main/java/com/sw/demoit/MyApp.java
@@ -1,8 +1,12 @@
 package com.sw.demoit;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.rong360.debug.view.DebugView;
+import com.rong360.debug.view.DebugViewWrapper;
 
 /**
  * Created by shiwang on 24/10/2017.
@@ -18,9 +22,15 @@ public class MyApp extends Application {
             ARouter.openDebug();
         }
         ARouter.init(this);
+        initDebugView();
     }
 
     private boolean isDebug() {
         return true;
+    }
+
+    private void initDebugView() {
+        DebugViewWrapper.getInstance().init(new DebugView.Builder(this).build());
+        DebugViewWrapper.getInstance().show();
     }
 }
