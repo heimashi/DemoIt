67d49916e0a242e219e1413d885f34d8fd9c4bc2
shiwang <shiwang@rong360.com>
2017-12-13 19:13:32 +0800

    fix bug

--- retrofit_demo/src/main/java/com/sw/retrofit/hello/RetrofitClient.java
+++ retrofit_demo/src/main/java/com/sw/retrofit/hello/RetrofitClient.java
@@ -1,6 +1,7 @@
 package com.sw.retrofit.hello;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
@@ -21,6 +22,7 @@ public class RetrofitClient {
             retrofitClient = new RetrofitClient();
             retrofitClient.retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
