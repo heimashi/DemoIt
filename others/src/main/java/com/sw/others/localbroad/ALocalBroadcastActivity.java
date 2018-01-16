7ba7bafecb35f072aecebd850ff89717886baef2
shiwang <shiwang@rong360.com>
2018-01-16 10:44:34 +0800

    update

--- others/src/main/java/com/sw/others/localbroad/ALocalBroadcastActivity.java
+++ others/src/main/java/com/sw/others/localbroad/ALocalBroadcastActivity.java
@@ -1,16 +1,25 @@
 package com.sw.others.localbroad;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.UUID;
 
 
 public class ALocalBroadcastActivity extends Activity {
@@ -26,7 +35,8 @@ public class ALocalBroadcastActivity extends Activity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ALocalBroadcastActivity.this, BLocalBroadcastActivity.class));
+                testID();
+                //startActivity(new Intent(ALocalBroadcastActivity.this, BLocalBroadcastActivity.class));
             }
         });
 
@@ -42,10 +52,112 @@ public class ALocalBroadcastActivity extends Activity {
         }
     }
 
+    private void testID() {
+        String uniqueID = UUID.randomUUID().toString();
+        Log.i("TEST", "++++uuid:" + uniqueID);
+        String ID = getUniquePsuedoID();
+        Log.i("TEST", "++++id:" + ID);
+
+        String android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.i("TEST", "++++aid:" + android_id);
+
+        Log.i("TEST", "++++Iid:" + Installation.id(this));
+
+    }
+
+
+
+    /**
+     * Return pseudo unique ID
+     *
+     * @return ID
+     */
+    public static String getUniquePsuedoID() {
+        // If all else fails, if the user does have lower than API 9 (lower
+        // than Gingerbread), has reset their device or 'Secure.ANDROID_ID'
+        // returns 'null', then simply the ID returned will be solely based
+        // off their Android device information. This is where the collisions
+        // can happen.
+        // Thanks http://www.pocketmagic.net/?p=1662!
+        // Try not to use DISPLAY, HOST or ID - these items could change.
+        // If there are collisions, there will be overlapping data
+        String m_szDevIDShort = "35" + (Build.BOARD.length() % 10) + (Build.BRAND.length() % 10) + (Build.CPU_ABI.length() % 10) + (Build.DEVICE.length() % 10) + (Build.MANUFACTURER.length() % 10) + (Build.MODEL.length() % 10) + (Build.PRODUCT.length() % 10);
+
+        // Thanks to @Roman SL!
+        // http://stackoverflow.com/a/4789483/950427
+        // Only devices with API >= 9 have android.os.Build.SERIAL
+        // http://developer.android.com/reference/android/os/Build.html#SERIAL
+        // If a user upgrades software or roots their device, there will be a duplicate entry
+        String serial = null;
+        try {
+            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+            Log.i("TEST", "++++serial:" + serial+" : "+Build.SERIAL);
+
+            // Go ahead and return the serial for api => 9
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception exception) {
+            // String needs to be initialized
+            serial = "serial"; // some value
+        }
+
+        // Thanks @Joe!
+        // http://stackoverflow.com/a/2853253/950427
+        // Finally, combine the values we have found by using the UUID class to create a unique identifier
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    }
+
+
+    public static class Installation {
+        private static String sID = null;
+        private static final String INSTALLATION = "INSTALLATION";
+
+        public synchronized static String id(Context context) {
+            if (sID == null) {
+                File installation = new File(context.getFilesDir(), INSTALLATION);
+                try {
+                    if (!installation.exists())
+                        writeInstallationFile(installation);
+                    sID = readInstallationFile(installation);
+                }
+                catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sID;
+        }
+
+        private static String readInstallationFile(File installation) throws IOException {
+            RandomAccessFile f = new RandomAccessFile(installation, "r");
+            byte[] bytes = new byte[(int) f.length()];
+            f.readFully(bytes);
+            f.close();
+            return new String(bytes);
+        }
+
+        private static void writeInstallationFile(File installation) throws IOException {
+            FileOutputStream out = new FileOutputStream(installation);
+            String id = UUID.randomUUID().toString();
+            out.write(id.getBytes());
+            out.close();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public static String getSerial() {
+        String serial = "";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            serial = Build.getSerial();
+        }else{
+            serial = Build.SERIAL;
+        }
+        return serial;
+    }
+
+
     private class MyBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i("TEST", "++++MyBroadcastReceiver onReceive:"+ Thread.currentThread().getName());
+            Log.i("TEST", "++++MyBroadcastReceiver onReceive:" + Thread.currentThread().getName());
             Toast.makeText(context, "收到LocalBroadcast", Toast.LENGTH_SHORT).show();
 
         }
